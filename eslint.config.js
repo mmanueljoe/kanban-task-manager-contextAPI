ecc100306e996cededc014b2331cc4817eafb072
mmanueljoe <emmanuelletsu18@gmail.com>
2026-01-30 23:53:22 +0000

    chore(linting): configure ESlint, Prettier, and pre-commit hooks

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,40 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import prettier from 'eslint-plugin-prettier';
+import react from 'eslint-plugin-react';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
-export default defineConfig([
+export default [
   globalIgnores(['dist']),
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  react.configs.flat.recommended,
+  reactHooks.configs.flat.recommended,
+  reactRefresh.configs.vite,
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
   },
-])
+  eslintConfigPrettier,
+  {
+    plugins: { prettier },
+    rules: {
+      'prettier/prettier': 'error',
+      'arrow-body-style': 'off',
+      'prefer-arrow-callback': 'off',
+    },
+  },
+  {
+    ignores: ['dist', 'node_modules', 'public'],
+  },
+];
